4ba35b6594355746802b251d486af9f6d24b2688
Vaibhav <vaibhavchaubey786@gmail.com>
2023-07-28 14:31:52 +0530

    Updated

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -18,7 +18,6 @@ const Movie = async () => {
   const res = await fetch(url, options);
   const data = await res.json();
   const main_data = data.titles;
-  console.log(data);
 
   return (
     <>
